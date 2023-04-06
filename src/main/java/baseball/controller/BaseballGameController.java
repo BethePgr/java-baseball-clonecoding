22bf238a5b31fa4464ca8a4a23916a8e72300e2e
SEC <c3392302@naver.com>
2023-04-06 11:36:03 +0900

    게임 실행 기능 구현

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -11,6 +11,9 @@ public class BaseballGameController {
 
     public void run(){
         runOneCycle();
+        if(choiceRestart()){
+            runOneCycle();
+        }
     }
 
     private void runOneCycle(){
@@ -21,10 +24,23 @@ public class BaseballGameController {
             GameResult result = new GameResult(computerNums,userNums);
             strikeCount = result.strikeCount;
             new OutputView(result).printGameResult();
-        }while(isGameEnd(strikeCount));
+        }while(!isGameEnd(strikeCount));
     }
 
     private boolean isGameEnd(int result){
         return result == 3;
     }
+
+    private boolean choiceRestart() throws IllegalArgumentException{
+        String userChoice = InputView.printInputRestartMessage();
+        if(!isRightInput(userChoice)){
+            throw new IllegalArgumentException();
+        }
+        return userChoice.equals("1");
+    }
+
+    private boolean isRightInput(String input){
+        return input.equals("1") || input.equals("2");
+    }
+
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static String INPUT_STRING_MESSAGE = "숫자를 입력해주세요 : ";
-
+    private static String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static String printInputNumberMessage(){
-        System.out.println(INPUT_STRING_MESSAGE);
+        System.out.println(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String printInputRestartMessage() {
+        System.out.println(INPUT_RESTART_MESSAGE);
         return Console.readLine();
     }
 }
