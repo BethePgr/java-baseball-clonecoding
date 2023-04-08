67fd19914811cb267a017410e343e9ebce4a7a87
jongwonpc <c3392302@naver.com>
2023-04-08 23:14:03 +0900

    게임 시작과 클리어 시 출력문 적용

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,7 @@ import baseball.domain.GameResult;
 
 public class OutputView {
 
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String BALL_MESSAGE = "볼";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
@@ -21,6 +22,9 @@ public class OutputView {
         printBallAndStrike();
     }
 
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
     public static void printClearMessage() {
         System.out.println(GAME_CLEAR_MESSAGE);
     }
