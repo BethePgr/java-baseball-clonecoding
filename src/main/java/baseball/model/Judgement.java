5ff5303360dc3a7c0c2041aedd0ea4271096b34d
SEC <c3392302@naver.com>
2023-06-05 15:22:46 +0900

    feat(judgement): 볼의 개수 판단 기능 구현

--- src/main/java/baseball/model/Judgement.java
+++ src/main/java/baseball/model/Judgement.java
@@ -23,4 +23,16 @@ public class Judgement {
             }
         }
     }
+
+    public void judgeBall(User user, Computer computer){
+        ballCount = 0;
+        List<Integer> userNumber = user.getUserInputNumber();
+        List<Integer> computerNumber = computer.getRandomNumber();
+
+        for(int i =0;i<3;i++){
+            if(userNumber.get(i) != computerNumber.get(i) && computerNumber.contains(userNumber.get(i))){
+                ballCount++;
+            }
+        }
+    }
 }
